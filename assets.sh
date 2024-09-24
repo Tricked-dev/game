57546c625c0c898559ad7fef6f5d07f678afc195
Tricked <tricked@tricked.pro>
2024-09-24 19:35:19 +0200

    favicon

--- assets.sh
+++ assets.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+output_dir=src/assets
+
 function layerAse2png {
-    aseprite -b --split-layers sprites/$1.aseprite --save-as public/assets/$1-{layer}.png
+    aseprite -b --split-layers sprites/$1.aseprite --save-as ${2:-$output_dir}/$1-{layer}.png
 }
 
 function ase2png {
-    aseprite -b sprites/$1.aseprite --save-as public/assets/$1.png
+    aseprite -b sprites/$1.aseprite --save-as ${2:-$output_dir}/$1.png
 }
+
 ase2png dice-bg
 ase2png number-base
 ase2png start-again
-ase2png background
+ase2png background static/assets
 ase2png deck-bg
 ase2png start-btn
 ase2png start-bg
@@ -21,6 +24,7 @@ ase2png options
 ase2png options
 ase2png save
 ase2png close
+ase2png favicon static/
 layerAse2png dices
 layerAse2png turns
 layerAse2png ending
